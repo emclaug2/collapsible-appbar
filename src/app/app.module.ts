e36d65bed4dd558ce1400c21eb2aab590960fc2a
joebochill <boyle.p.joseph@gmail.com>
2019-07-09 09:02:08 -0400

    Fix goofy styling and remove unused files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,20 +19,17 @@ import {
   MatGridListModule
 } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {ScrollDispatchModule} from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
-import { GroupByPipe } from './shared/groupby.pipe';
-import { KeyValuePipe } from './shared/key-value.pipe';
-import { UnCamelCasePipe } from './shared/un-camel-case.pipe';
+
 
 @NgModule({
   imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule,
   MatExpansionModule,
     MatButtonModule, MatIconModule,
-    MatTabsModule, FlexLayoutModule,ScrollDispatchModule,
+    MatTabsModule, FlexLayoutModule,
   MatFormFieldModule, MatCardModule,MatListModule, MatGridListModule],
-  declarations: [AppComponent, GroupByPipe, KeyValuePipe, UnCamelCasePipe],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
