7dc8c8e2e313e427ed626b29a2b43b0e4d02f974
Chinna Gurrala <cm.reddy071@gmail.com>
2019-10-28 13:48:33 -0400

    created ionic local collapsiblae-appbar example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { presidents } from './shared/presidents';
+import * as Colors from '@pxblue/colors';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  list = presidents;
+  private readonly SHRINK_TOP_SCROLL_POSITION = 130;
+  shrinkToolbar = false;
+  scrollingSubscription: any;
+  scrollTop: any;
+  Colors: Object = Colors;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -22,6 +32,11 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
     });
+  }
+
+  logScrolling(scrollTop: any) {
+    this.shrinkToolbar = scrollTop.detail.scrollTop > this.SHRINK_TOP_SCROLL_POSITION ? true : false;
   }
 }
---
+++ src/app/expandable-header.ts
@@ -0,0 +1,48 @@
+import { Component, Input, ElementRef, Renderer2 } from '@angular/core';
+import { DomController } from '@ionic/angular';
+
+@Component({
+  selector: 'expandable-header',
+  template: '<ng-content></ng-content>',
+})
+export class ExpandableHeader {
+
+  @Input('scrollArea') scrollArea: any;
+  @Input('headerHeight') headerHeight: number;
+  @Input('shrinkToolbar') shrinkToolbar: boolean;
+
+
+  newHeaderHeight: any;
+
+  constructor(public element: ElementRef, public renderer: Renderer2, public domCtrl: DomController) {
+
+  }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.element.nativeElement, 'height', this.headerHeight + 'px');
+    this.scrollArea.ionScroll.subscribe((ev) => {
+      this.resizeHeader(ev);
+    });
+
+  }
+
+  resizeHeader(ev: any) {
+    if (ev) {
+      this.domCtrl.write(() => {
+        this.newHeaderHeight = this.headerHeight - ev.detail.scrollTop;
+        if (this.newHeaderHeight < 56) {
+          this.newHeaderHeight = 56;
+        }
+        this.renderer.setStyle(this.element.nativeElement, 'height', this.newHeaderHeight + 'px');
+        if (document.getElementById('mainTitle')) {
+          if ((this.newHeaderHeight / 3.5) < 30) {
+            document.getElementById('mainTitle').style.fontSize = (this.newHeaderHeight / 3.5) + 'px';
+          }
+        }
+        if (document.getElementById('subTitle')) {
+          document.getElementById('subTitle').style.fontSize = (this.newHeaderHeight / 12.5) + 'px';
+        }
+      });
+    }
+  }
+}
